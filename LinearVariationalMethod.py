3ccd10a730f996db6ca511c9e0441273fff17a7a
Remm97 <eldabaghr@student.wpunj.edu>
2019-02-01 14:18:04 -0500

    Add files via upload

--- LinearVariationalMethod.py
+++ LinearVariationalMethod.py
@@ -1,27 +1,37 @@
 import numpy as np
-import sympy as sp
 
 def H_ij(i,j):
     a = (2/10)*(np.sin((i*np.pi)/2)*np.sin((j*np.pi)/2))
-    b = (((np.pi**2)*(j**2))/((1000)))
-    x = sp.Symbol('x')
-    c = np.sin((i*np.pi*x)/10)
-    d = np.sin((j*np.pi*x)/10)
-    e = sp.integrate((c)*(d), x)
-    ham_int = a+(b*e)
+    b = (((np.pi**2)*(j**2))/((200)))
+
+    ham_int = a+b
     
     if i==j:
-        e = 0
+        ham_int = a+b
     
     else:
-        e = 1
+        ham_int = a
     
     return ham_int
 
 H_mat = np.zeros((3,3))
 
-for i in range(0,3):
-    for j in range(0,3):
-        H_mat[i][j] = H_ij(i,j)
+for i in range(1,4):
+    for j in range(1,4):
+        H_mat[i-1][j-1] = H_ij(i,j)
         
 print(H_mat)
+
+c = np.zeros(3)
+
+c[0] = 0
+c[1] = 0
+c[2] = np.sqrt(.5)
+
+norm = np.dot(np.transpose(c), c)
+
+Hc = np.dot(H_mat,c)
+
+E = np.dot(np.transpose(c),Hc)/norm
+
+print(E)
